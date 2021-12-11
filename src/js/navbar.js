afe221ae3d9de4996fbe85338570ea0dbc15afd2
Loren Waddle <lwaddle@gmail.com>
2021-12-10 19:01:20 -0800

    Create working collapse navbar

---
+++ src/js/navbar.js
@@ -0,0 +1,17 @@
+const navbarTogglerButton = document.querySelector(".navbar__toggler-button");
+const navbarTogglerButtonIcon = document.querySelector(".navbar__toggler-button-icon");
+const navbarCollapseContent = document.querySelector(".navbar__collapse-content");
+
+let togglerStateExpanded = false;
+
+navbarTogglerButton.addEventListener("click", function() {
+    if (!togglerStateExpanded) {
+        navbarCollapseContent.classList.add("navbar__collapse-content--show");
+        navbarTogglerButtonIcon.classList.add("navbar__toggler-button-icon--active");
+        togglerStateExpanded = true;
+    } else {
+        navbarCollapseContent.classList.remove("navbar__collapse-content--show");
+        navbarTogglerButtonIcon.classList.remove("navbar__toggler-button-icon--active");
+        togglerStateExpanded = false;
+    }
+});
